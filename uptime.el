37039351cc39ce53b26226029812d40c25b99bea
Mark M. Scala <markscala@gmail.com>
2011-11-27 20:12:41 -0600

    initial development commit

---
+++ uptime.el
@@ -0,0 +1,36 @@
+;;; uptime.el: keeps a persistent record of your best emacs-uptime.
+
+(unless (file-exists-p "~/.emacs.d/.uptime")
+  (save-excursion
+    (let ((buf (find-file-noselect "~/.emacs.d/.uptime")))
+      (set-buffer buf)
+      (erase-buffer)
+      (print (list 'setq 'best-uptime 0) buf)
+      (save-buffer)
+      (kill-buffer))))
+
+(load "~/.emacs.d/.uptime")
+
+(defun save-uptime ()
+  "Save the best uptime into .uptime."
+  (save-excursion
+    (let ((buf (find-file-noselect "~/.emacs.d/.uptime")))
+      (set-buffer buf)
+      (erase-buffer)
+      (print (list 'setq 'best-uptime (string-to-number (emacs-uptime "%s"))) buf)
+      (save-buffer)
+      (kill-buffer))))
+
+(defun compare-uptimes ()
+  "Update *best-uptime* if necessary."
+  (let ((uptime (string-to-number (emacs-uptime "%s"))))
+    (if (> uptime best-uptime)
+	(save-uptime))))
+	  
+(add-hook 'kill-emacs-hook 'compare-uptimes)
+
+(defun report-uptime ()
+  "Display best uptime in mini-buffer"
+  (interactive)
+  (message (format "Your best uptime is: %s" 
+		   (format-seconds "%D, %z%.2h:%.2m:%.2s" best-uptime))))
